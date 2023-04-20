34109a59d93f9e5a2dfc3da1dc57708d9954907d
mgarciaCS <gar.mari2020@gmail.com>
2023-04-20 12:58:29 -0700

    added new tests and fixed bugs for reversedInPlace and reversed

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,24 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = { 3, 4, 5, 6};
+
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
-	}
+
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{6,5,4,3}, input2);
+  }
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 =  { 3, 4, 5, 6};
+
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+   
+    assertArrayEquals(new int[]{6,5,4,3}, ArrayExamples.reversed(input2));
   }
 }
